930de63abf69aa93514473f6ea4cf4da6038ba75
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-30 14:53:15 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-30.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
