670fd175c834bf9b04f4a88f0fd48ef40a11f97e
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-31 20:53:55 -0500

    Got a basic static Interface created.

--- Views/LandingView.swift
+++ Views/LandingView.swift
@@ -23,9 +23,24 @@ struct LandingView: View {
         NavigationView {
             VStack {
                 List{
-                    Text("Study for Chemistry quiz")
-                    Text("Finish Computer Science Assignment")
-                    Text("Go for a run around campus")
+                    Label(
+                        title: {
+                        Text("Study for Chemistry quiz") },
+                        icon: { Image(systemName: "circle")
+                        })
+                    
+                    Label(
+                        title: {
+                        Text("Finish Computer Science assignment") },
+                        icon: { Image(systemName: "circle")
+                        })
+                    
+                    Label(
+                        title: {
+                        Text("Go for a run around campus") },
+                        icon: { Image(systemName: "circle")
+                        })
+
                 }
                 .searchable(text: $searchText)
                 
